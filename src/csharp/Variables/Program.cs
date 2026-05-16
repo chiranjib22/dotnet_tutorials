7e79d33bca41b59c02f7237f7dc104b09f5154d0
chiranjib22 <chiranjib.chakraborty.cse@gmail.com>
2026-05-16 14:17:55 +0600

    variable is completed

--- src/csharp/Variables/Program.cs
+++ src/csharp/Variables/Program.cs
@@ -7,19 +7,37 @@
 internal class Program
 {
     public static int x = 100; // static variable
-    public int y = 200; // non static variable
+    public int y; // non static variable
+    public const float PI = 3.14f; // constant variable, behaviour same as static variable
 
-    public Program(int a)
+    // difference is that constant variable must be initialized at the time of declaration and cannot be changed later,
+    // whereas static variable can be assigned a value at any time and can be changed later
+
+    public readonly int r = 10; // readonly variab
+
+    public Program(int a,int r)
     {
         y = a; // assigning value to non static variable in constructor
+        this.r = r; // assigning value to readonly variable in constructor
     }
     static void Main(string[] args)
     {
         int z; // static variable
         Console.WriteLine($"Static variable x: {x}");
-        var obj = new Program(10);
-        var obj2 = new Program(20);
+        Console.WriteLine($"Constant variable PI: {PI}");
+        var obj = new Program(10,30);
+        var obj2 = new Program(20,40);
         Console.WriteLine($"Non static variable y: {obj.y}");
+        Console.WriteLine($"Readonly variable r: {obj.r}");
         Console.WriteLine($"Non static variable y: {obj2.y}");
+        Console.WriteLine($"Readonly variable r: {obj2.r}");
+        obj.Method();
+    }
+
+    private void Method()
+    {
+        int localVariable; // local variable, only accessible within this method
+        localVariable = 10;
+        Console.WriteLine($"Local variable: {localVariable}");
     }
 }
