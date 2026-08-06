b22d1f87f55408a58819f4105868b659065c1431
chiranjib22 <chiranjib.chakraborty.cse@gmail.com>
2026-08-06 16:30:39 +0600

    control statement completed

--- src/csharp/ControlStatement/Program.cs
+++ src/csharp/ControlStatement/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// By Default program statement are excuted sequentially line by line from TOP to BOTTOM of the solution file
+
+//Console.WriteLine("Executing Statment1");
+//Console.WriteLine("Executing Statment2");
+//Console.WriteLine("Executing Statment3");
+//Console.WriteLine("Executing Statment4");
+//Console.WriteLine("Executing Statment5");
+
+// we can skip the excution of some statement based on condition 
+
+//if(10 > 5)
+//{
+//    Console.WriteLine("Executing Statment1");
+//    Console.WriteLine("Executing Statment2");
+//}else
+//{
+//    Console.WriteLine("Executing Statment3");
+//    Console.WriteLine("Executing Statment4");
+//}
+//Console.WriteLine("Executing Statment5");
+
+// we can repeatedly execute some statement 
+
+//for(int i = 0; i<5; i++)
+//{
+//    Console.WriteLine("Welcome to C#.NET");
+//}
+//Console.WriteLine("Some Other Statement");
+
+// we can jump to any statement bellow or top 
+
+Console.WriteLine("Executing Statment1");
+goto STATEMENT4;
+Console.WriteLine("Executing Statment2");
+
+//STATEMENT3:
+Console.WriteLine("Executing Statment3");
+
+STATEMENT4:     // label
+Console.WriteLine("Executing Statment4");
+//goto STATEMENT3;
+
+// Control Statement are 4 types
+// 1. Selection Statement or Branching Statement. if..else, switch
+// 2. Iteration Statement or Looping Statement. while, do while, for, foreach
+// 3. Jumping Statement. goto, break, continue, return
---
+++ src/csharp/HashTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+// what is problem with ArrayList (to get the data i have to remember the index position)
+ArrayList al = new ArrayList();
+
+al.Add(1001);
+al.Add("James");
+al.Add("Manager");
+al.Add(3500);
+al.Add("Mumbai");
+al.Add("IT");
+al.Add("a@a.com");
+
+for(int i = 0; i< al.Count; i++)
+{
+    Console.WriteLine($"al[{i}] = {al[i]}");
+}
+
