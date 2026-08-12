994ef89cf0342725d26fd50ce8bee2b4b0d42bfc
Chiranjib Chakraborty <chiranjib@akijbashir.com>
2026-08-12 12:48:43 +0600

    function is started to read

---
+++ src/csharp/Functions/Program.cs
@@ -0,0 +1,21 @@
+﻿// build in function
+
+int number = 25;
+double squareRoot = Math.Sqrt(number);
+Console.WriteLine(squareRoot);
+
+// user defined function
+// <Access Specifier> [Modifier] <Return Types> <Function Name>([Parameter List])
+{
+    // function Body / Method Body
+}
+
+// 1. Function Name : Mandatory.
+// 2. Parameter List : Optional
+// 3. Return Type : Mandatory. Can not return multiple values. Can return no values (void)
+// 4. Access Specifier: Optional. Scope : private, public, protected
+// 5. Modifier : Optional. Access Type : static, virtual, parital, sealed.
+//                                       static  : accessing directly without creating its instance
+//                                       virtual : means override the function and changed its logic with same prototype
+//                                       parital : split the defination
+// 6. Function Body: Mandatory
---
+++ src/csharp/Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿// Loop statements are called repetative or iterative statements.
+// 2 types of loops : 
+// 1. Counter Loops
+// 2. Conditional Loops
+
+// 4 types of loops exist in C#:
+// 1. For Loop
+// 2. While Loop
+// 3. Do While Loop
+// 4. For Each Loop
+
+
+for(int i = 1; i<=10; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+// Loops are divided into 2 categories:
+// 1. Entry Controlled Loops : For, While, For Each
+// 2. Exit Controlled Loops : Do While
+
+
